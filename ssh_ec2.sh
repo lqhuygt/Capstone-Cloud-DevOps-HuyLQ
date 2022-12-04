5a9e76d26eee7fd53b6be733772f7a58a56cd132
lqhuygtt <lqhuygt@gmail.com>
2022-12-04 10:43:57 +0700

    deploy k8s to ec2

--- ssh_ec2.sh
+++ ssh_ec2.sh
@@ -4,7 +4,6 @@ spawn ssh -i "huylq23-keypair.pem" ec2-user@ec2-52-87-223-167.compute-1.amazonaw
 expect "Are you sure" { send "yes\r" } 
 expect "ec2-user" { send "kubectl get nodes\r" }
 expect "ec2-user" { send "kubectl run capstone-kube --image=huylq1999/capstone_aws_devops --port=5000\r" }
-expect "ec2-user" { send "sleep 180\r" }
 expect "ec2-user" { send "kubectl get pods\r" }
 expect "ec2-user" { send "kubectl port-forward capstone-kube 5000:5000\r" }
 expect "ec2-user" { send "logout\r" } 

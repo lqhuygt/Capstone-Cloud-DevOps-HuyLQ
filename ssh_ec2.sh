d9dc9c47a66b71817d7b4409472dede3fe14135a
lqhuygtt <lqhuygt@gmail.com>
2022-12-04 09:22:31 +0700

    deploy k8s to ec2

--- ssh_ec2.sh
+++ ssh_ec2.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/expect -f
 
-spawn ssh -i "huylq23-keypair.pem" ec2-user@ec2-44-211-148-59.compute-1.amazonaws.com
+spawn ssh -i "huylq23-keypair.pem" ec2-52-87-223-167.compute-1.amazonaws.com
 expect "Are you sure" { send "yes\r" } 
 expect "ec2-user" { send "aws --version\r" }
-expect "ec2-user" { send "eksctl create cluster -f eks_cluster.yml\r" }
-expect "ec2-user" { send "kubectl get nodes\r" }
-expect "ec2-user" { send "kubectl run capstone-kube --image=huylq1999/capstone_aws_devops --port=5000\r" }
-expect "ec2-user" { send "sleep 180\r" }
-expect "ec2-user" { send "kubectl get pods\r" }
-expect "ec2-user" { send "kubectl port-forward capstone-kube 5000:5000\r" }
+expect "ec2-user" { send "eksctl version\r" }
+expect "ec2-user" { send "kubectl version\r" }
+# expect "ec2-user" { send "eksctl create cluster -f eks_cluster.yml\r" }
+# expect "ec2-user" { send "kubectl get nodes\r" }
+# expect "ec2-user" { send "kubectl run capstone-kube --image=huylq1999/capstone_aws_devops --port=5000\r" }
+# expect "ec2-user" { send "sleep 180\r" }
+# expect "ec2-user" { send "kubectl get pods\r" }
+# expect "ec2-user" { send "kubectl port-forward capstone-kube 5000:5000\r" }
 expect "ec2-user" { send "logout\r" } 
 interact
